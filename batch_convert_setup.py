27e690eb03b33489fb8075a93d7d6b659e197783
langgaibo <gabe.langton@gmail.com>
2015-07-27 15:36:11 -0700

    Cleanup

--- batch_convert_setup.py
+++ batch_convert_setup.py
@@ -1,26 +1,30 @@
-#test logic for executing a shell script to process a batch of .mov files
-#with ffmpeg
+# Written off the cuff by Gabe Langton 2015
 from os import listdir
 from os.path import isfile, join
 
-contents = [ f for f in listdir('/Users/gabe/Movies/Batch') if isfile(join('/Users/gabe/Movies/Batch',f))]
+# Make sure to specify the absolute path to /Batch in both places here.
+contents = [ f for f in listdir('.../Batch') if isfile(join('.../Batch',f))]
 
 print contents
-print "\n\n\n\n"
+print "\n\n"
 
+# Modify these cleaners with the video file extension of your choice.
 cleaned = filter(lambda m: '.MOV' in m, contents)
 cleaned_lower = filter(lambda m: '.mov' in m, contents)
+
+# In my common use case, different apple devices result in different caps
+# for the .mov extension... so this just normalizes them into a single list.
 for f in cleaned_lower:
     upped = f.replace(".mov", ".MOV")
     cleaned.append(upped)
 
+# Opens and overwrites the contenst of the commands file
 batch_text = open('staged_commands.txt', 'w')
-
-print "New contents of staged_commands.txt"
-
 for n in cleaned:
+# This is my most common argument set for ffmpeg. Please review the ffmpeg
+# documentation and substitute your own arguments for your own batch.
+# Especially note that the ""-crf" option is not universal. There are other
+# quality commands for other file types!!!
     line = 'ffmpeg -i %s -crf 30 -threads 2 %s\n' %(n, n.replace(".MOV", ".mp4"))
-    print line
     batch_text.write(line)
-
 batch_text.close()
