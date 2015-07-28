c2c64e86020c868530cc60192d67ea87d3b16ac2
langgaibo <gabe.langton@gmail.com>
2015-07-27 23:14:17 -0700

    One last cleanup of README for even clearer instructions.

--- Batch/batch_convert_setup.py
+++ Batch/batch_convert_setup.py
@@ -3,22 +3,23 @@ from os import listdir
 from os.path import isfile, join
 
 # Make sure to specify the absolute path to /Batch in both places here.
-contents = [ f for f in listdir('.../Batch') if isfile(join('.../Batch',f))]
+contents = [ f for f in listdir('/Users/gabe/Movies/Bucket/Batch') if isfile(join('/Users/gabe/Movies/Bucket/Batch',f))]
 
 print contents
 print "\n\n"
 
 # Modify these cleaners with the video file extension of your choice.
+# Make sure to change the extension below, in the ffmpeg arguments %s substitution
 cleaned = filter(lambda m: '.MOV' in m, contents)
-cleaned_lower = filter(lambda m: '.mov' in m, contents)
 
 # In my common use case, different apple devices result in different caps
 # for the .mov extension... so this just normalizes them into a single list.
+cleaned_lower = filter(lambda m: '.mov' in m, contents)
 for f in cleaned_lower:
     upped = f.replace(".mov", ".MOV")
     cleaned.append(upped)
 
-# Opens and overwrites the contenst of the commands file
+# Opens and overwrites the contents of the commands file
 batch_text = open('staged_commands.txt', 'w')
 for n in cleaned:
 # This is my most common argument set for ffmpeg. Please review the ffmpeg
