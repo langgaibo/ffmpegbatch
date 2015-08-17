13267c14abf7fc1fa2e4a6f5dc0a36746b591767
langgaibo <gabe.langton@gmail.com>
2015-08-17 10:34:28 -0700

    Proper shell script for batch_time.sh, with instructions

--- Batch/batch_time.sh
+++ Batch/batch_time.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+# if you want to run this from anywhere in the command line
+# uncomment the line below and change to the absolute path/Batch.
+
+#cd .../Batch
 python batch_convert_setup.py
 source staged_commands.txt
