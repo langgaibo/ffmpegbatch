c9d0a7cff0d5e6c77069bb4977d22ebdd8719485
Gabe Langton <gabe.langton@gmail.com>
2020-09-14 10:47:48 -0700

    merge conflict junk sat in here for 5 years

--- Batch/convert.py
+++ Batch/convert.py
@@ -26,13 +26,12 @@ for f in lower:
 # open and overwrite a text file with a list of ffmpeg commands
 batch_text = open('staged_commands.txt', 'w')
 for n in cleaned:
-<<<<<<< HEAD
     # IMPORTANT: see ffmpeg documentation for guidance on quality settings.
     # here, '-crf 26' represents a high compression level, at the expense of slight degradation in video quality.
     # Normal "sane" range for -crf is 18 (visually lossless) to 28 (often 10x filesize reduction with acceptable quality)
+
     line = 'ffmpeg -i %s -crf 26 -threads 2 %s\n' %(n, n.replace(".MOV", ".mp4"))
     batch_text.write(line)
-=======
 
     if ".MOV" in n:
         # Write the new output video to a subdirectory
@@ -50,5 +49,4 @@ for n in cleaned:
         sweep = 'mv %s ./originals/%s\n' % (n, n)
         batch_text.write(sweep)
 
->>>>>>> 88450b322a58b6e88d661557490fea0565fc3f5f
 batch_text.close()
